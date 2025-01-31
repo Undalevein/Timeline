7e0650108c3ef0ac312193b46ff8d2ce6a6aae06
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2025-01-30 21:54:37 -0800

    Added bettwe PowerShell Test

--- test.ps1
+++ test.ps1
@@ -10,8 +10,8 @@ function Assert-MatchTests {
 
 function Assert-Equals {
     param (
-        [Parameter(Mandatory = $true, ValueFromPipeline)] $Output,
-        [Parameter(Mandatory = $true)] $Expected
+        [Parameter(Mandatory = $true)]$Expected,
+        [Parameter(Mandatory = $true, ValueFromPipeline)]$Output
     )
     
     if ($Output -ne $Expected) {
@@ -20,24 +20,28 @@ function Assert-Equals {
 }
 
 # Timeline Interpreter Location
-$interp = "$PSScriptRoot\TimelineInterpreter.java"
+$interp = "$PSScriptRoot\src\TimelineInterpreter.java"
 
 $Error.clear()
-Get-Content "$PSScriptRoot\bad_printing.timeline" | java -ea "$interp" |
+Get-Content "$PSScriptRoot\examples\bad_printing.timeline" | java -ea "$interp" |
     Assert-Equals ("AMORPHOUSAMORPHOUSAMORPHOUSUNEVALUATEDAAAA") &&
-Get-Content "$PSScriptRoot\circle_effect.timeline" | java -ea "$interp" |
+Get-Content "$PSScriptRoot\examples\circle_effect.timeline" | java -ea "$interp" |
     Assert-Equals ("0123") &&
-# Get-Content "$PSScriptRoot\hello_world.timeline" | java -ea "$interp" |
+Get-Content "$PSScriptRoot\examples\dropoff_rigorous.timeline" | java -ea "$interp" |
+    Assert-Equals ("AABACADAEAFAGAHAIAJA") &&
+Get-Content "$PSScriptRoot\examples\dropoff.timeline" | java -ea "$interp" |
+    Assert-Equals ("10") &&
+# Get-Content "$PSScriptRoot\examples\hello_world.timeline" | java -ea "$interp" |
 #     Assert-Equals "Hello, World!" &&
-Get-Content "$PSScriptRoot\hi.timeline" | java -ea "$interp" |
+Get-Content "$PSScriptRoot\examples\hi.timeline" | java -ea "$interp" |
     Assert-Equals ("Hi") &&
-Get-Content "$PSScriptRoot\horizontal_looping.timeline" | java -ea "$interp" |
+Get-Content "$PSScriptRoot\examples\horizontal_looping.timeline" | java -ea "$interp" |
     Assert-Equals ("00") &&
-Get-Content "$PSScriptRoot\infinity_cell_loop.timeline" | java -ea "$interp" |
+Get-Content "$PSScriptRoot\examples\infinity_cell_loop.timeline" | java -ea "$interp" |
     Assert-Equals ("012345678901234567890123456789") &&
-Get-Content "$PSScriptRoot\math_operators.timeline" | java -ea "$interp" |
+Get-Content "$PSScriptRoot\examples\math_operators.timeline" | java -ea "$interp" |
     Assert-Equals ("4") &&
-Get-Content "$PSScriptRoot\vertical_looping.timeline" | java -ea "$interp" |
+Get-Content "$PSScriptRoot\examples\vertical_looping.timeline" | java -ea "$interp" |
     Assert-Equals ("a") &&
 ForEach-Object 'foo';
 
